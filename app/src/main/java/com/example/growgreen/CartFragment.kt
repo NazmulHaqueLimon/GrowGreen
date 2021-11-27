b2017221c1973b110df8917952dd26c137727528
User <nh.limon6600@gmail.com>
2021-11-27 23:45:05 +0600

    UI updated

--- app/src/main/java/com/example/growgreen/CartFragment.kt
+++ app/src/main/java/com/example/growgreen/CartFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.growgreen.databinding.FragmentCartBinding
 import com.example.growgreen.databinding.FragmentProductsBinding
 
@@ -37,8 +38,10 @@ class CartFragment : Fragment() {
 
     }
 
+
     private fun deleteItem(it: MyProducts) {
         viewModel.removeCart(it)
+        findNavController().navigateUp()
     }
 
 }
